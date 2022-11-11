db7c89cb035953f03c84edfdb613fe07f45e67eb
paulhmorris <paulh.morris@gmail.com>
2022-11-11 12:38:28 -0600

    Add coords column and migration

---
+++ prisma/migrations/20221110163038_add_coords/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `endLat` on the `Ride` table. All the data in the column will be lost.
+  - You are about to drop the column `endLon` on the `Ride` table. All the data in the column will be lost.
+  - You are about to drop the column `startLat` on the `Ride` table. All the data in the column will be lost.
+  - You are about to drop the column `startLon` on the `Ride` table. All the data in the column will be lost.
+
+*/
+-- create postgis extension
+CREATE EXTENSION postgis;
+
+-- AlterTable
+ALTER TABLE "Ride" DROP COLUMN "endLat",
+DROP COLUMN "endLon",
+DROP COLUMN "startLat",
+DROP COLUMN "startLon",
+ADD COLUMN     "coords" geometry(Point, 4326);
+
+-- CreateIndex
+CREATE INDEX "location_idx" ON "Ride" USING GIST ("coords");
